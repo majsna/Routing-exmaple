178e0f1070149060214cce9daeb9161f4ae01690
Mateusz Meisner <mat.meisner@gmail.com>
2018-05-17 12:11:44 +0200

    finished reactive form course

--- routing-example/src/app/about/reactive-form/reactive-form.component.ts
+++ routing-example/src/app/about/reactive-form/reactive-form.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup} from "@angular/forms";
+import {AbstractControl, FormArray, FormControl, FormGroup, ValidationErrors, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-reactive-form',
@@ -9,16 +9,61 @@ import {FormControl, FormGroup} from "@angular/forms";
 export class ReactiveFormComponent implements OnInit {
 
   contactForm: FormGroup;
+  message = new ReactiveMessage();
   courses = ['Angular', 'JavaFx', 'Java8', 'OrmLite'];
 
   ngOnInit() {
     this.contactForm = new FormGroup({
-      topic: new FormControl('ropo'),
-      message: new FormControl(null),
-      name: new FormControl(null),
-      email: new FormControl(null),
-      course: new FormControl(this.courses[3]),
-      bot: new FormControl(),
+      topic: new FormControl(null, Validators.required),
+      message: new FormControl(null, [Validators.required, Validators.minLength(50)]),
+      name: new FormControl(null, Validators.required),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      course: new FormControl(this.courses[0]),
+      bot: new FormControl(null),
+      questions: new FormArray([new FormControl(null), new FormControl(null)], this.questionsValidator)
+    });
+
+    // this.contactForm.valueChanges.subscribe(value => {
+    //   console.log(value);
+    // })
+
+    this.contactForm.get('topic').valueChanges.subscribe(value => {
+      console.log(value);
+    })
+
+    this.contactForm.get('email').statusChanges.subscribe(value => {
+      console.log(value);
+    })
+  }
+
+  addQuestion() {
+    const arr = <FormArray>this.contactForm.get('questions');
+    arr.push(new FormControl(null));
+  }
+
+  questionsValidator(control: AbstractControl): ValidationErrors{
+    const arr = <string>control.value;
+    if(arr.includes('angularjs')){
+      return {forbiddenCourse: true};
+    }
+  }
+
+  onSubmit() {
+    console.log(this.contactForm);
+    this.message.topic = this.contactForm.get('topic').value;//another way to get the value
+    this.message.message = this.contactForm.value.message;
+    this.message.name = this.contactForm.value.name;
+    this.message.email = this.contactForm.value.email;
+    this.message.course = this.contactForm.value.course;
+    this.message.bot = this.contactForm.value.bot;
+    console.log(this.message);
+    this.onReset();
+  }
+
+  onReset(){
+    this.contactForm.reset({
+      // topic: 'Nice course',
+      course: this.courses[0]
     });
   }
 
