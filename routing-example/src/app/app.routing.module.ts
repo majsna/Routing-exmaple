bed7c47d872646bf33fd7d132a95cb8c3d4af67b
Mateusz Meisner <mat.meisner@gmail.com>
2018-06-28 16:04:30 +0200

    lazy loading

--- routing-example/src/app/app.routing.module.ts
+++ routing-example/src/app/app.routing.module.ts
@@ -7,7 +7,17 @@ import {PageNotFoundModule} from "./page-not-found/page-not-found.module";
 import {SecretModule} from "./secret/secret.module";
 import {AuthModule} from "./auth/auth.module";
 
-const appRoutes: Routes = [];
+const appRoutes: Routes = [
+
+  // { path: '', loadChildren: './home/home.module#HomeModule'},
+  // { path: 'courses', loadChildren: './courses/courses.module#CoursesModule'},
+  // { path: 'about', loadChildren: './about/about.module#AboutModule'},
+  // { path: 'login', loadChildren: './auth/auth.module#AuthModule'},
+  // { path: 'secret', loadChildren: './secret/secret.module#SecretModule'},
+  // { path: 'not-found', loadChildren: './page-not-found/page-not-found.module#PageNotFoundModule'},
+  // { path: '**', redirectTo: '/not-found'}
+
+];
 
 @NgModule({
   imports: [
